8b29ee92552c1ae0c6af05f7708e50497f1454ac
Patrick Auld <pauld@Birst.com>
2013-07-26 11:18:57 -0700

    Correctly configure JerseyServletModule to find root resources

--- src/main/java/com/patrickauld/karyon/jersey/CrudJerseyModule.java
+++ src/main/java/com/patrickauld/karyon/jersey/CrudJerseyModule.java
@@ -1,5 +1,8 @@
 package com.patrickauld.karyon.jersey;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 
@@ -7,9 +10,8 @@ public class CrudJerseyModule extends JerseyServletModule {
 
   @Override
   protected void configureServlets() {
-    bind(CrudRoot.class).asEagerSingleton();
-    bind(CrudResource.class).asEagerSingleton();
-
-    serve("/*").with(GuiceContainer.class);
+    Map<String, String> params = Maps.newHashMap();
+    params.put("com.sun.jersey.config.property.packages","com.patrickauld.karyon");
+    serve("/*").with(GuiceContainer.class, params);
   }
 }
